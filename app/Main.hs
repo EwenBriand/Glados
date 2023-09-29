5af0aa4470fadd1bd2c25501112ea5245d315850
Eliot Janvier <eliot.janvier@epitech.eu>
2023-09-29 12:03:34 +0200

    update: instruction table now has the latest instructions

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,10 @@
 module Main (main) where
 
 import REPL
-import MySyscall(
-    SyscallCode(..),
-    execSyscall)
 import VM
 import Lexer
 import EvaluateAST
 
 main :: IO ()
-main = do
-    let c = astNodeArrayToHASM (Just (newContext)) (ASTNodeArray [ASTNodeInteger 1, ASTNodeInteger 2])
-    case c of
-        Nothing -> putStrLn "Error!"
-        Just ctx -> print (instructions ctx)
-
+main = runREPL
 
