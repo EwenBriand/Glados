d23bebb41bd516858690d0e3aff6bad5aca5a37f
Eliot Janvier <eliot.janvier@epitech.eu>
2023-10-17 12:00:30 +0200

    add: constructor for default ELF

--- hello.asm
+++ hello.asm
@@ -1,20 +1,13 @@
-; FILEPATH: /hello.asm
-
 section .data
-    hello db 'Hello, world!', 0
-
+    ; no data yet
 section .text
     global _start
 
 _start:
-    ; write hello message to stdout
-    mov rax, 1          ; syscall for write
-    mov rdi, 1          ; stdout file descriptor
-    mov rsi, hello      ; message to write
-    mov rdx, 13         ; message length
-    syscall
-
-    ; exit program with status code 0
-    mov rax, 60         ; syscall for exit
-    xor rdi, rdi        ; exit status code 0
-    syscall
+    xor rax, rax
+    mov rax, 1
+    push rax
+    xor rax, rax
+    mov rax, 2
+    pop rdi
+    add rax, rdi
--- src/MakeELF.hs
+++ src/MakeELF.hs
@@ -1,3 +1,14 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving  #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module MakeELF
   (
     debugLoadAndShowElf,
@@ -10,43 +21,107 @@ import Data.ByteString.Lazy as BL
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy.Char8 as BLC
 import Data.Elf
--- import Data.Singletons
--- import Data.Singletons.TypeLits
 import Numeric (showHex)
 import Data.Elf.PrettyPrint (printElf, printElf_, readFileLazy)
+import Data.Elf.Constants
+import Data.Elf.Headers
+import Data.Proxy
+import Data.Bits
+import Data.ByteString.Lazy.Internal
+
+import VM
 
 loadBin :: FilePath -> IO BL.ByteString
 loadBin path = do
   bin <- B.readFile path
   return $ BL.fromStrict bin
 
-
--- loadElf :: MonadCatch m => BL.ByteString -> m Elf
--- loadElf = parseElf
-
--- printElf :: ElfList c -> IO ()
--- printElf elfList = do
---   case elfList of
---     ElfList elfXXList -> do
---       -- Assuming the ELF header is the first element of the list
---       case elfXXList of
---         (elf : _) -> do
---           Prelude.putStrLn "ELF Header:"
---           Prelude.putStrLn $ "  Magic: " ++ show (ehOSABI elf)
---           Prelude.putStrLn $ "  Type: " ++ show (ehType elf)
---           Prelude.putStrLn $ "  Machine: " ++ show (ehMachine elf)
---           Prelude.putStrLn $ "  Version: " ++ show (ehABIVersion elf)
---           Prelude.putStrLn $ "  Entry point address: 0x" ++ showHex (ehEntry elf) ""
---           Prelude.putStrLn $ "  Flags: " ++ showHex (ehFlags elf) ""
---         _ -> Prelude.putStrLn "No ELF header found"
-
 debugPrintJustElf :: Elf -> IO ()
 debugPrintJustElf elf = print "oui le type est ok"
+
+easyPrintElf :: Elf -> IO ()
+easyPrintElf elf = do
+  doc <- printElf_ False elf
+  print doc
 
 debugLoadAndShowElf :: FilePath -> IO ()
 debugLoadAndShowElf path = do
     bs <- fromStrict <$> BS.readFile path
     elf <- parseElf bs
-    doc <- printElf_ False elf
-    print doc
+    easyPrintElf loadDefaultElf
+    -- easyPrintElf elf
+    -- doc <- printElf_ False elf
+    -- print doc
 
+
+-- | Create an ElfLabel from a its name and a bool that is true if the symbol is to be global.
+-- It is assumed that:
+--  - the symbol is in the .text section which is the first section of the elf
+--  - the symbol is a function
+makeElfLabel :: String -> Bool -> ElfSymbolXX 'ELFCLASS64
+makeElfLabel name isGlobal = let
+        steName = name
+        steBind = if isGlobal then STB_Global else STB_Local
+        steType = STT_NoType
+        steShNdx = 1
+        steValue = 0
+        steSize = 0
+    in ElfSymbolXX {..}
+
+serializeSymbol :: ElfSymbolXX 'ELFCLASS64 -> B.ByteString
+serializeSymbol ElfSymbolXX {..} = B.concat
+    [
+        serializeWord32 (fromIntegral $ fromEnum steName)
+        , serializeWord8 (fromIntegral $ fromEnum steBind)
+        , serializeWord8 (fromIntegral $ fromEnum steType)
+        , serializeWord16 (fromIntegral steShNdx)
+        , serializeWord64 steValue
+        , serializeWord64 steSize
+    ]
+
+-- | Load a default ELF file for an x64 machine, without any symbols or code.
+loadDefaultElf :: Elf
+loadDefaultElf = Elf SELFCLASS64 $
+        ElfHeader
+            { ehData       = ELFDATA2LSB
+            , ehOSABI      = ELFOSABI_SYSV
+            , ehABIVersion = 0
+            , ehType       = ET_REL
+            , ehMachine    = EM_AARCH64
+            , ehEntry      = 0
+            , ehFlags      = 0
+            }
+        ~: ElfSection {
+            esName        = ".text"
+            , esType      = SHT_PROGBITS
+            , esFlags     = SHF_EXECINSTR .|. SHF_ALLOC
+            , esAddr      = 0
+            , esAddrAlign = 8
+            , esEntSize   = 0
+            , esN         = 0
+            , esLink      = 0
+            , esInfo      = 0
+            , esData      = ElfSectionData Data.ByteString.Lazy.Internal.Empty -- update this with the program's code
+            }
+        ~: ElfSection {
+            esName         = ".symtab"
+            , esType       = SHT_SYMTAB
+            , esFlags      = 0
+            , esAddr       = 0
+            , esAddrAlign  = 8
+            , esEntSize    = 24 -- not 18 like in hello.o? investigate
+            , esN          = 0
+            , esLink       = 0
+            , esInfo       = 0
+            , esData       = serializeSymbol $ makeElfLabel "_start" True
+            }
+        ~: ElfSectionTable
+        ~: ElfListNull
+
+
+
+updateElfFromContext :: Context -> Elf -> Elf
+updateElfFromContext context elf = elf -- todo change the elf
+contextToElf :: Context -> Elf
+contextToElf context =
+    updateElfFromContext context loadDefaultElf
